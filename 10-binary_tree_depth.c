7a1b91035a2f66fc94242351fdb22a7443c6b069
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-04-18 17:23:39 +0000

    a few more protos

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,3 +1,10 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_depth - Measures the depth of a node in a binary tree.
+ * @tree: Pointer to the node to measure the depth.
+ * Return: if tree is NULL, 0.
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,3 +1,10 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_leaves - Counts the leaves in a binary tree.
+ * @tree: Pointer to the root node of the tree to count the number of leaves.
+ * Return: If tree is NULL, 0.
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
