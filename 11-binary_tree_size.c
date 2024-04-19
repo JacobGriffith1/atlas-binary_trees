f7a8bc00e3b3adcd9136de88deef1fee9da8fe0c
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-04-19 06:39:20 +0000

    11-1: Only in code can you define the size of a tree with a single number

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -3,8 +3,19 @@
 /**
  * binary_tree_size - Measures the size of a binary tree.
  * @tree: Pointer to the root node of the tree to measure the size.
- * Return: If tree is NULL, 0.
+ * Return: Size.
  */
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
+	size_t size = 0, r = 0, l = 0;
+
+	if (!tree)
+		return (0);
+
+	l = binary_tree_size(tree->left);
+	r = binary_tree_size(tree->right);
+	size = r + l + 1;
+
+	return (size);
+}
