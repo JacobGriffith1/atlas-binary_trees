0b950c5cbd15bd302403b34724d47592aecd0ebc
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-04-19 23:52:09 +0000

    16-9: A wild shot, I doubt it will work, but if it does cool

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -14,6 +14,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	if (!tree->left && !tree->right)
 		return (1);
+	if (!tree->left ^ !tree->right)
+		return (0);
 
 	if (tree->left && tree->right)
 	{
