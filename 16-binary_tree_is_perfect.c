d4c407de9537af8e87c3e5d7e2d1e6595ccbf197
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-04-19 23:42:55 +0000

    16-8: Back to the start, but with the single node check just after the null tree check

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -12,6 +12,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
 
 	if (tree->left && tree->right)
 	{
@@ -21,7 +23,5 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 			return (1);
 		return (0);
 	}
-	if (!tree->left && !tree->right)
-		return (1);
 	return (0);
 }
