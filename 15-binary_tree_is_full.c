5a0106dbba8c99c822c7e0b4fc628f2a9a4285c2
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-04-19 22:22:18 +0000

    15-1: full of bologna

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -3,8 +3,23 @@
 /**
  * binary_tree_is_full - Checks if binary tree is full
  * @tree: A pointer to the root node of the tree to check
- * Return: If tree is NULL, 0
+ * Return: Full ? 1 : 0
  */
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
+	int l = 0, r = 0;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left && tree->right)
+	{
+		l = binary_tree_is_full(tree->left);
+		r = binary_tree_is_full(tree->right);
+		if (l == 0 || r == 0)
+			return (0);
+		return (1);
+	}
+	return ((!tree->left && !tree->right) ? 1 : 0);
+}
