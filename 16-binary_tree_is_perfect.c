fd5a1bac60d41114d493df992d97734974776890
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-04-19 22:48:49 +0000

    16-1: Perfection is relative

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -3,8 +3,23 @@
 /**
  * binary_tree_is_perfect - Checks if a binary tree is perfect.
  * @tree: A pointer to the root node of the tree to check.
- * Return: If tree is NULL, 0.
+ * Return: Perfect ? 1 : 0
  */
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	int l = 0, r = 0;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left && tree->right)
+	{
+		l = 1 + binary_tree_is_perfect(tree->left);
+		r = 1 + binary_tree_is_perfect(tree->right);
+		if (l == r && l != 0 && r != 0)
+			return (1);
+		return (0);
+	}
+	return (0);
+}
