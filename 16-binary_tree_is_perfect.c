c1bf97a781fffe4aab180c91690333cc9c5c3eca
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-04-20 00:04:51 +0000

    16-X: Rework with aux func

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,29 +1,43 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_perfect - Checks if a binary tree is perfect.
- * @tree: A pointer to the root node of the tree to check.
+ * perfect - Auxilary for binary_tree_is_perfect
+ * @tree: Tree
+ * Return: Height
+ */
+
+int perfect(const binary_tree_t *tree)
+{
+	int l = 0, r = 0;
+
+	if (tree->left && tree->right)
+	{
+		l = 1 + perfect(tree->left);
+		r = 1 + perfect(tree->right);
+		if (r == l && r != 0 && l != 0)
+			return (r);
+		return (0);
+	}
+	if (!tree->left && !tree->right)
+		return (1);
+	return (0);
+}
+
+/**
+ * binary_tree_is_perfect - Checks if a binary tree is perfectly balanced
+ * @tree: Tree
  * Return: Perfect ? 1 : 0
  */
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int l = 0, r = 0;
+	int p = 0;
 
 	if (!tree)
 		return (0);
-	if (!tree->left && !tree->right)
-		return (1);
-	if (!tree->left ^ !tree->right)
-		return (0);
 
-	if (tree->left && tree->right)
-	{
-		l = 1 + binary_tree_is_perfect(tree->left);
-		r = 1 + binary_tree_is_perfect(tree->right);
-		if (l == r && l != 0 && r != 0)
-			return (1);
-		return (0);
-	}
+	p = perfect(tree);
+	if (p != 0)
+		return (1);
 	return (0);
 }
